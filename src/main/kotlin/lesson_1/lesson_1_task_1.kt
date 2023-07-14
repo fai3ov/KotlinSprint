431594a483cd8de6ffdf39e06994559ad1d1db12
BENIHANA <d.re4d@yandex.ru>
2023-07-14 19:32:19 +0300

    KS-1-1 created 2 variables: totalNumberOfOrders, thankYouMessage

---
+++ src/main/kotlin/lesson_1/lesson_1_task_1.kt
@@ -0,0 +1,6 @@
+package lesson_1
+
+fun main() {
+    val totalNumberOfOrders: Int = 75
+    val thankYouMessage: String = "Спасибо за ваш заказ! Мы ценим ваш выбор и стремимся сделать ваш опыт покупок лучше с каждым разом."
+}
