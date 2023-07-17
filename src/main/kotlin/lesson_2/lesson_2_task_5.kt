b282248235bed2e0afdb4f3b6061062d8f46b391
BENIHANA <d.re4d@yandex.ru>
2023-07-17 14:46:19 +0300

    KS-2-4

---
+++ src/main/kotlin/lesson_2/lesson_2_task_5.kt
@@ -0,0 +1,12 @@
+package lesson_2
+fun main() {
+    // формула сложного процента A = P * (1 + r)^t
+
+    val P = 70_000.0
+    val r = 16.7 / 100
+    val t = 20.0
+
+    val A = P * Math.pow(1 + r, t)
+
+    println(String.format("%.3f", A))
+}
