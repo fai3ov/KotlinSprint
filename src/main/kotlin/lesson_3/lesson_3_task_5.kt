4193196acb40d33e6752ec53bbb347778b1d5106
BENIHANA <d.re4d@yandex.ru>
2023-07-18 18:01:56 +0300

    KS-3-5

---
+++ src/main/kotlin/lesson_3/lesson_3_task_5.kt
@@ -0,0 +1,15 @@
+package lesson_3
+
+fun main() {
+    val moveString = "D2-D4;0"
+
+    val parts = moveString.split("-", ";")
+
+    val from = parts[0]
+    val to = parts[1]
+    val moveNumber = parts[2].toInt()
+
+    println("Откуда: $from")
+    println("Куда: $to")
+    println("Номер хода: $moveNumber")
+}
