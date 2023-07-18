18878f49c8d083b54ab29fe2919fd719fb71d76f
BENIHANA <d.re4d@yandex.ru>
2023-07-18 17:20:58 +0300

    KS-3-1

---
+++ src/main/kotlin/lesson_3/lesson_3_task_1.kt
@@ -0,0 +1,11 @@
+package lesson_3
+
+fun main() {
+    val userName = "Иван"
+
+    var greeting = "Добрый день, $userName!"
+    println(greeting)
+
+    greeting = "Добрый вечер, $userName!"
+    println(greeting)
+}
